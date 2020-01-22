53b925054b4f2fcb3f193647336ac65ba4f6f4c3
Abbe Keultjes <flauwekeul@gmail.com>
2020-01-22 21:20:44 +0100

    add login form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import './App.css'
 
-export default () => <div></div>
+export default () => (
+  <div className="login">
+    <Form>
+      <h1 className="mb-4">Actuarium</h1>
+      <p className="text-muted">To join a game, enter your name and a game code.</p>
+      <Form.Group controlId="name">
+        <Form.Label srOnly>Your name</Form.Label>
+        <Form.Control type="text" size="lg" placeholder="Your name" />
+      </Form.Group>
+      <Form.Group controlId="code">
+        <Form.Label srOnly>Game code</Form.Label>
+        <Form.Control type="text" size="lg" placeholder="Game code" autoFocus />
+      </Form.Group>
+      <Button type="submit" variant="primary" block size="lg">
+        Start
+      </Button>
+    </Form>
+    <Button variant="link" block className="mt-5">
+      I'm a teacher
+    </Button>
+  </div>
+)
