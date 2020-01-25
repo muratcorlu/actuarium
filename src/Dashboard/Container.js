51dd56aa2b2ca99f3bd052f4335233c4fe9d011c
Abbe Keultjes <flauwekeul@gmail.com>
2020-01-25 16:01:44 +0100

    add Container component that will wrap the pages for admins and players

---
+++ src/Dashboard/Container.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Navbar from 'react-bootstrap/Navbar'
+import { useSelector } from 'react-redux'
+import { useFirebase } from 'react-redux-firebase'
+
+const Container = ({ children }) => {
+  const firebase = useFirebase()
+  const auth = useSelector(state => state.firebase.auth)
+
+  return (
+    <>
+      <Navbar variant="dark" bg="dark">
+        <Navbar.Brand>Actuarium</Navbar.Brand>
+        <Navbar.Text className="ml-auto mr-2">{auth.displayName}</Navbar.Text>
+        <Button size="sm" variant="outline-secondary" onClick={firebase.logout}>
+          Logout
+        </Button>
+      </Navbar>
+      {children}
+    </>
+  )
+}
+
+export default Container
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button'
-import Navbar from 'react-bootstrap/Navbar'
 import { useSelector } from 'react-redux'
-import { isEmpty, isLoaded, useFirebase } from 'react-redux-firebase'
+import { isEmpty, isLoaded } from 'react-redux-firebase'
 import Login from '../Login'
+import Container from './Container'
 
 const Dashboard = () => {
   const auth = useSelector(state => state.firebase.auth)
-  const firebase = useFirebase()
 
   if (!isLoaded(auth)) {
     // todo: display loading state
@@ -18,15 +16,7 @@ const Dashboard = () => {
     return <Login />
   }
 
-  return (
-    <Navbar variant="dark" bg="dark">
-      <Navbar.Brand>Actuarium</Navbar.Brand>
-      <Navbar.Text className="ml-auto mr-2">{auth.displayName}</Navbar.Text>
-      <Button size="sm" variant="outline-secondary" onClick={firebase.logout}>
-        Logout
-      </Button>
-    </Navbar>
-  )
+  return <Container></Container>
 }
 
 export default Dashboard
